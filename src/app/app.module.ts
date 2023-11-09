9af2c46c07d2c315e09a6db3ddcd1cdbae1cc8d8
3rooks <brookslynx@gmail.com>
2023-11-09 02:18:19 -0300

    🐛 x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { SlidesComponent } from './slides/slides.component';
-import { AppSlideshowComponent } from './app-slideshow/app-slideshow.component';
 
 @NgModule({
-    declarations: [AppComponent, SlidesComponent, AppSlideshowComponent],
+    declarations: [AppComponent],
     imports: [BrowserModule],
     providers: [],
     bootstrap: [AppComponent],
